ece3476753b8419aa20b7b477b59b1f498a30655
sowmaki <solene.tassel@hotmail.com>
2025-01-20 16:48:35 +0100

    style des header et footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,5 +1,6 @@
 import { Link } from 'react-router-dom'
-import logo from '../../assets/logo.svg'
+import logo from '../../assets/logo-white.svg'
+import './Footer.scss'
 
 function Footer() {
   return (
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,11 +1,12 @@
 import { Link } from 'react-router-dom'
 import logo from '../../assets/logo.svg'
+import './Header.scss'
 
 function Header() {
   return (
     <header className='header'>
       <Link to="/"><img className="header__logo" src={logo} alt="logo kasa" /></Link>
-      <nav>
+      <nav className='header__nav'>
         <Link to="/" className='header__link'>Accueil</Link>
         <Link to="/about" className='header__link'>A Propos</Link>
       </nav>
