7873ad0c4649e6f3d6f907b5d09dfbb7d92fead7
sowmaki <solene.tassel@hotmail.com>
2025-03-15 13:03:32 +0100

    suppression d'un proptypes

--- src/components/LogementsWrapper/index.jsx
+++ src/components/LogementsWrapper/index.jsx
@@ -1,17 +1,15 @@
-import PropTypes from "prop-types";
 import LogementCard from "../LogementCard";
 import '../LogementsWrapper/LogementsWrapper.scss';
 
 const LogementsWrapper = ({ logements }) => {
   return (
     <ul className="logements-wrapper">
-      {logements.map(logement => <li className="logement-card" key={logement.id}><LogementCard id={logement.id} cover={logement.cover} title={logement.title} /></li>)}
+      {logements.map(logement =>
+        <li className="logement-card" key={logement.id}>
+          <LogementCard id={logement.id} cover={logement.cover} title={logement.title} />
+        </li>)}
     </ul>
   )
-}
-
-LogementsWrapper.propTypes = {
-  logements: PropTypes.string,
 }
 
 
