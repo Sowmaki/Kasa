f64dbce89f9e4965632b4c322843a2f015c68d60
sowmaki <solene.tassel@hotmail.com>
2025-03-15 14:43:16 +0100

    modification underline liens

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,14 +1,16 @@
-import { Link } from 'react-router-dom'
-import logo from '../../assets/logo.svg'
-import './Header.scss'
+import { Link, useLocation } from 'react-router-dom';
+import logo from '../../assets/logo.svg';
+import './Header.scss';
 
 function Header() {
+  const location = useLocation();
+
   return (
     <header className='header'>
       <Link to="/"><img className="header__logo" src={logo} alt="logo kasa" /></Link>
       <nav className='header__nav'>
-        <Link to="/" className='header__link'>Accueil</Link>
-        <Link to="/about" className='header__link'>A Propos</Link>
+        <Link to="/" className={`header__link ${location.pathname === "/" ? "active" : ""}`}>Accueil</Link>
+        <Link to="/about" className={`header__link ${location.pathname === "/about" ? "active" : ""}`}>A Propos</Link>
       </nav>
     </header>
   )
